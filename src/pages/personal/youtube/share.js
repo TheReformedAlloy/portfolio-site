26bb143a2c266dd9c5bc205c2cd9dec16b814d13
Client444 <the.reformed.alloy@gmail.com>
2021-11-17 15:59:12 -0500

    Removed Stripe.

--- src/pages/personal/youtube/share.js
+++
@@ -1,9 +0,0 @@
-const stripe = require('stripe')('sk_test_51GqMw4K3tUUGyNxKXpn7FMJVz93RpRNvCPj0Nc9JXLQgSLdmXWIXOPNBDGSGs8tkxRMAMzv1WMvGtBEy65AZIQHW00wGQivpPh');
-
-export default function Share() {
-    return (
-        <>
-            
-        </>
-    )
-}
