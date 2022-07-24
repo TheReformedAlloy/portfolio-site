574a2375a010c7e22128d7eb37e79e8d0376868b
Clint Mooney <the.reformed.alloy@gmail.com>
2022-07-24 03:10:04 -0400

    Add sw from next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
     images: {
       domains: ['s3.amazonaws.com'],
       layoutRaw: true,
     },
-  }
+  });
